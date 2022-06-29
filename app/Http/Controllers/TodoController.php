939e6c9b186615b6efbfdcc37d74d898f763490c
Alireza Moammer <moameralireza@gmail.com>
2022-06-29 22:03:14 +0430

    It was added three routes(( update,show and destroy))

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -22,4 +22,28 @@ class TodoController extends Controller
        ]);
        return response()->json($request,Response::HTTP_CREATED);
    }
+
+   public function update($id ,Request $request){
+       $todo=Todo::findOrFail($id);
+       $todo->update([
+           'title'=>$request->title,
+           'description'=>$request->description
+       ]);
+       return response()->json($todo,Response::HTTP_OK);
+   }
+
+   public function destroy($id){
+       $todo=Todo::findOrFail($id);
+       $todo->delete();
+       $result=[
+           'status'=>'success',
+           'message'=> 'ToDo deleted successfully'
+       ];
+       return response()->json($result,Response::HTTP_OK);
+   }
+
+   public function show($id){
+       $todo=Todo::findOrFail($id);
+       return response()->json($todo,Response::HTTP_OK);
+   }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/v1/todos',[App\Http\Controllers\TodoController::class,'index']);
 Route::post('/v1/todos',[App\Http\Controllers\TodoController::class,'store']);
+Route::patch('/v1/todos/{id}',[App\Http\Controllers\TodoController::class,'update']);
+Route::delete('/v1/todos/{id}',[App\Http\Controllers\TodoController::class,'destroy']);
+Route::get('/v1/todos/{id}',[App\Http\Controllers\TodoController::class,'show']);
