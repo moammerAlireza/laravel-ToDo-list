cacb3577f8f1000a73c8e8a6851a8ea1faf96970
Alireza Moammer <moameralireza@gmail.com>
2022-06-30 18:19:59 +0430

    It was added error handling

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -12,41 +12,55 @@ use Illuminate\Http\Response;
 
 class TodoController extends Controller
 {
-   public function index(){
-       $todo= Todo::paginate(8);
-       return new TodoCollection($todo,'Todos received successfully');
-   }
+    public function index()
+    {
+        $todo = Todo::paginate(8);
+        return new TodoCollection($todo, 'Todos received successfully');
+    }
 
-   public function store(Request $request){
+    public function store(Request $request)
+    {
 
-       $todo = Todo::create([
-           'title'=>$request->title,
-           'description' => $request->description
-       ]);
-       return new TodoResource($todo,'Todo created successfully');
-   }
+        $todo = Todo::create([
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+        if (!$todo) {
+            return response()->json(["message" => "failed"], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return new TodoResource($todo, 'Todo created successfully');
+    }
 
-   public function update($id ,Request $request){
-       $todo=Todo::findOrFail($id);
-       $todo->update([
-           'title'=>$request->title,
-           'description'=>$request->description
-       ]);
-       return new TodoResource($todo,"Todo Updated successfully.");
-   }
+    public function update($id, Request $request)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->update([
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+        if (!$todo) {
+            return response()->json(['message' => 'failed'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return new TodoResource($todo, "Todo Updated successfully.");
+    }
 
-   public function destroy($id){
-       $todo=Todo::findOrFail($id);
-       $todo->delete();
-       $result=[
-           'status'=>'success',
-           'message'=> 'ToDo deleted successfully'
-       ];
-       return response()->json($result,Response::HTTP_OK);
-   }
+    public function destroy($id)
+    {
+        $todo = Todo::findOrFail($id);
+        $result = $todo->delete();
+        if (!$result) {
+            return response()->json(['message' => 'failed'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        $result = [
+            'status' => 'success',
+            'message' => 'ToDo deleted successfully'
+        ];
+        return response()->json($result, Response::HTTP_OK);
+    }
 
-   public function show($id){
-       $todo=Todo::findOrFail($id);
-       return new TodoResource($todo,"todo received successfully");
-   }
+    public function show($id)
+    {
+        $todo = Todo::findOrFail($id);
+        return new TodoResource($todo, "todo received successfully");
+    }
 }
