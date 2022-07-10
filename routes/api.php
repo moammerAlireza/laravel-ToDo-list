9879c529e6232953ff9b750dbbf669ea39d6d8d0
Alireza Moammer <moameralireza@gmail.com>
2022-07-10 13:47:07 +0430

    It has been added localization using middleware with header

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,14 @@ use App\Http\Controllers\TodoController;
 use App\Http\Controllers\AuthController;
 
 
-Route::post('/v1/user/register',[AuthController::class,'register']);
-Route::post('/v1/user/login',[AuthController::class,'login']);
+Route::post('/v1/user/register',[AuthController::class,'register'])->middleware(['switch_language']);
+Route::post('/v1/user/login',[AuthController::class,'login'])->middleware(['switch_language']);
 
-Route::group(['prefix'=>'/v1/user','middleware'=>'auth:sanctum'], function (){
+Route::group(['prefix'=>'/v1/user','middleware'=>['auth:sanctum','switch_language']], function (){
     Route::get('/logout',[AuthController::class,'logout']);
 });
 
-Route::group(['prefix'=>'/v1/todos','middleware'=>'auth:sanctum'], function (){
+Route::group(['prefix'=>'/v1/todos','middleware'=>['auth:sanctum','switch_language']], function (){
 
     Route::get('/',[TodoController::class,'index']);
     Route::post('/',[TodoController::class,'store']);
